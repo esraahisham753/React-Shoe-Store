4a3ab30a2314816ad92fbcaa60b129eae19ef0b2
Esraa Abduallah <esraahisham753@gmail.com>
2023-06-21 13:40:13 +0000

    Complete Products Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from "./services/productService";
+import Products from "./Products";
 
 export default function App() {
-  const [size, setSize] = useState("");
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    getProducts("shoes").then((response) => {
-      setProducts(response);
-    });
-  }, []);
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-
-  const filteredProducts = size
-    ? products.filter((product) => {
-        return product.skus.find((sku) => {
-          return sku.size === parseInt(size);
-        });
-      })
-    : products;
-
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select
-              id="size"
-              value={size}
-              onChange={(e) => {
-                setSize(e.target.value);
-              }}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            {size && <h2>Found {filteredProducts.length} items</h2>}
-          </section>
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+          <Products />
         </main>
       </div>
       <Footer />
