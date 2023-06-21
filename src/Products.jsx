1e6ae1af8e0407e12f4d0be0bed8cf3e4f6fdbbb
Esraa Abduallah <esraahisham753@gmail.com>
2023-06-21 17:03:43 +0000

    Add Routes

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Products() {
   const [size, setSize] = useState("");
+  const { category } = useParams();
   const {
     data: products,
     error,
