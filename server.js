4e7a30c4d0a0868b7d40c2c7c678bc0f6e66985d
Esraa Abduallah <esraahisham753@gmail.com>
2023-07-12 11:26:33 +0000

    add /products and /products/:id to express server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require("express");
+var cors = require("cors");
+var db = require("./db.json");
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/products", (req, res) => {
+  let category = req.query.category;
+  res.send(db["products"].filter((p) => p.category === category));
 });
 
-app.listen(3001, () => console.log("Example app listening on port 3001!"));
+app.get("/products/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let items = db["products"].filter((p) => p.id === id);
+  res.send(items[0]);
+});
+
+app.post("/shippingAddress", (req, res) => {});
+
+app.listen(3001, () => console.log(`Example app listening on port 3001!`));
