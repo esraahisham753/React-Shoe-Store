3c43cbbe710c9b4495da5db74000f9c566aca104
Esraa Abduallah <esraahisham753@gmail.com>
2023-07-23 10:37:27 +0000

    Use Details functional component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
-import Details from "./Details.class";
+import Details from "./Details";
 import Cart from "./Cart";
 import Checkout from "./Checkout.class";
 import { useCart } from "./cartContext";
